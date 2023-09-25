75b7d9251ed964938e6a8effcd950a68991f8c06
gserena01 <serenagandhi1@gmail.com>
2023-09-25 17:33:13 -0400

    I drew a triangle

--- first_app.h
+++ first_app.h
@@ -3,18 +3,41 @@
 #include "lve_window.h"
 #include "lve_pipeline.h"
 #include "lve_device.h"
+#include "lve_swap_chain.h"
+#include "lve_model.h"
 
-namespace lve {
-class FirstApp {
- public:
-  static constexpr int WIDTH = 800;
-  static constexpr int HEIGHT = 600;
+#include <memory>
+#include <vector>
 
-  void run();
+namespace lve
+{
+  class FirstApp
+  {
+  public:
+    static constexpr int WIDTH = 800;
+    static constexpr int HEIGHT = 600;
 
- private:
-  LveWindow lveWindow{WIDTH, HEIGHT, "Hello Vulkan!"};
-  LveDevice lveDevice{lveWindow};
-  LvePipeline lvePipeline{lveDevice, "shaders/simple_shader.vert.spv", "shaders/simple_shader.frag.spv", LvePipeline::defaultPipelineConfigInfo(WIDTH, HEIGHT)};
-};
-}  // namespace lve
+    FirstApp();
+    ~FirstApp();
+
+    FirstApp(const FirstApp &) = delete;
+    FirstApp &operator=(const FirstApp &) = delete;
+
+    void run();
+
+  private:
+    void loadModels();
+    void createPipelineLayout();
+    void createPipeline();
+    void createCommandBuffers();
+    void drawFrame();
+
+    LveWindow lveWindow{WIDTH, HEIGHT, "Hello Vulkan!"};
+    LveDevice lveDevice{lveWindow};
+    LveSwapChain lveSwapChain{lveDevice, lveWindow.getExtent()};
+    std::unique_ptr<LvePipeline> lvePipeline;
+    VkPipelineLayout pipelineLayout;
+    std::vector<VkCommandBuffer> commandBuffers;
+    std::unique_ptr<LveModel> lveModel;
+  };
+} // namespace lve
