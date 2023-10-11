45ade38937c1bc43bbb816a4ff46611fa3c1ac46
gserena01 <serenagandhi1@gmail.com>
2023-10-11 09:17:56 -0400

    add viewprojection

--- 3D_Engine/simple_render_system.cpp
+++ 3D_Engine/simple_render_system.cpp
@@ -66,6 +66,8 @@ void SimpleRenderSystem::renderGameObjects(
     const LveCamera& camera) {
   lvePipeline->bind(commandBuffer);
 
+  auto projectionView = camera.getProjection() * camera.getView();
+
   for (auto& obj : gameObjects) {
     obj.transform.rotation.y = glm::mod(obj.transform.rotation.y + 0.001f, glm::two_pi<float>());
     obj.transform.rotation.x = glm::mod(obj.transform.rotation.x + 0.0005f, glm::two_pi<float>());
@@ -73,8 +75,8 @@ void SimpleRenderSystem::renderGameObjects(
 
     SimplePushConstantData push{};
     push.color = obj.color;
-    push.transform = camera.getProjection() * obj.transform.mat4();
-    
+    push.transform = projectionView * obj.transform.mat4();
+
     vkCmdPushConstants(
         commandBuffer,
         pipelineLayout,
